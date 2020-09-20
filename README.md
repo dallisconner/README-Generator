50d40629cf18d6c40f51a6771a3f5008316499fd
8bitk9 <67807672+dallisconner@users.noreply.github.com>
2020-09-19 23:03:56 -0400

    Basically works, has some quirks.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-#Yeahhh
-Yeahhh
+#readme gen
+##Table of Contents
+[object Object]
+
+##Description
+makes readmes
+
+##Usage
+run the thing
+
+##Installation
+plug it in
+
+##License
+MIT
+
+##Tests
+so many
+
+##Contributing
+give me money
+
+##Questions
+DC
+DC@dc.io
--- index.js
+++ index.js
@@ -2,13 +2,20 @@ var inquirer = require("inquirer");
 var fs = require("fs");
 
 // array of formatted user responses
-var answers = [];
+var answers = ["","","","","","","",""];
 
 //function to format user responses
 function writeAnswers(response)
 {
 
-  answers[0] = response.title;
+  answers[0] = "##Description" + '\n' + response.description + '\n';
+  answers[1] = "##Table of Contents" + '\n' + response + '\n';
+  answers[2] = "##Installation" + '\n' + response.installation + '\n';
+  answers[3] = "##Usage" + '\n' + response.usage + '\n';
+  answers[4] = "##License" + '\n' + response.license + '\n';
+  answers[5] = "##Contributing" + '\n' + response.contributing + '\n';
+  answers[6] = "##Tests" + '\n' + response.tests + '\n';
+  answers[7] = "##Questions" + '\n' + response.questionsGH + '\n' + response.questionsEmail;
 
 }
 
@@ -44,11 +51,42 @@ function init() {
       name: "description"
     },
     {
+      type: "input",
+      message: "Please detail instructions for the proper installation of this application:",
+      name: "installation"
+    },
+    {
+      type: "input",
+      message: "Please detail usage instructions for this application:",
+      name: "usage"
+    },
+    {
       type: "list",
       message: "Please select a license:",
       choices:["MIT","GPL","Apache"],
       name: "license"
+    },
+    {
+      type: "input",
+      message: "Please describe any methods with which to contribute to the development of this application:",
+      name: "contributing"
+    },
+    {
+      type: "input",
+      message: "Please describe any tests to be performed on this application:",
+      name: "tests"
+    },
+    {
+      type: "input",
+      message: "Please provide your GitHub username:",
+      name: "questionsGH"
+    },
+    {
+      type: "input",
+      message: "Please provide a contact email address:",
+      name: "questionsEmail"
     }
+
   ])
   .then(function(response) {
 
